77ae6224c8791780bce227a15731238ad068c71c
saparhadi <inilhohadi@gmail.com>
2018-12-11 17:27:35 +0700

    Update shopify_app.rb

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,8 +1,8 @@
 shopify_app_config = Rails.application.config_for(:shopify_app)
 
 ShopifyApp.configure do |config|
-  config.api_key = shopify_app_config.fetch('api_key')
-  config.secret = shopify_app_config.fetch('secret')
+  config.api_key = shopify_app_config.fetch('fe60080408208f25fb559bb3400df7cb')
+  config.secret = shopify_app_config.fetch('ab5672c6aa13a4fa889a8652becb74fb')
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
